da21d7b04312c1d5700858ecacc7a8630c02783d
sagarshah95 <shah.sagar@northeastern.edu>
2020-06-14 16:28:32 -0400

    Udagram

---
+++ exercises/udacity-c2-restapi/src/config/config.ts
@@ -0,0 +1,22 @@
+export const config = {
+  "dev": {
+    "username": "udagramsagardev",
+    "password": "udagramsagardev",
+    "database": "udagramsagardev",
+    "host": "udagramsagardev.cgvjkvp78r5g.us-east-2.rds.amazonaws.com",
+    "dialect": "postgress",
+    "aws_region": "us-east-2",
+    "aws_profile": "default",
+    "aws_media_bucket": "udagram-sagar-dev"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  },
+  "jwt": {
+    "secret": "helloworld"
+  }
+}
